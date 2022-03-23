115cbceff8fe59003fa40404ab0e4d3400c91c49
Tanish2002 <tanishkhare@gmail.com>
2022-03-23 16:36:22 +0530

    testing

--- controllers/register.go
+++ controllers/register.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"attendance-backend/models"
 	"attendance-backend/services"
 	"fmt"
+	"strconv"
 )
 
 func RegisterFace(name string, gender string, company_id uint, imagePath string) error {
@@ -10,13 +12,12 @@ func RegisterFace(name string, gender string, company_id uint, imagePath string)
 	if faceId != "" {
 		return fmt.Errorf("face already registered")
 	}
-	fmt.Println("HELO!!!!!!!!!!!!!")
-	// id, err := models.AddEmployeeDetails(name, gender, company_id)
-	// if err != nil {
-	// 	return err
-	// }
-	// if err := services.RegisterFace(imagePath, strconv.FormatUint(uint64(id.ID), 10)); err != nil {
-	// 	return err
-	// }
+	id, err := models.AddEmployeeDetails(name, gender, company_id)
+	if err != nil {
+		return err
+	}
+	if err := services.RegisterFace(imagePath, strconv.FormatUint(uint64(id.ID), 10)); err != nil {
+		return err
+	}
 	return nil
 }
--- handlers/register.go
+++ handlers/register.go
@@ -1,31 +1,27 @@
 package handlers
 
 import (
-	"attendance-backend/controllers"
-	"fmt"
 	"net/http"
-	"net/http/httputil"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/cast"
 )
 
 func RegisterFaceHandler(c *gin.Context) {
-	req, _ := httputil.DumpRequest(c.Request, true)
-	fmt.Println(string(req))
-	name := c.PostForm("name")
-	gender := c.PostForm("gender")
-	company_id_query := c.PostForm("company_id")
-	company_id := cast.ToUint(company_id_query)
-	image, err := c.FormFile("image")
+	// req, _ := httputil.DumpRequest(c.Request, true)
+	// fmt.Println(string(req))
+	// name := c.PostForm("name")
+	// gender := c.PostForm("gender")
+	// company_id_query := c.PostForm("company_id")
+	// company_id := cast.ToUint(company_id_query)
+	_, err := c.FormFile("image")
 	if err != nil {
 		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
-	c.SaveUploadedFile(image, "/tmp/image.jpg")
-	if err := controllers.RegisterFace(name, gender, company_id, "/tmp/image.jpg"); err != nil {
-		c.String(http.StatusServiceUnavailable, err.Error())
-		return
-	}
+	// c.SaveUploadedFile(image, "/tmp/image.jpg")
+	// if err := controllers.RegisterFace(name, gender, company_id, "/tmp/image.jpg"); err != nil {
+	// 	c.String(http.StatusServiceUnavailable, err.Error())
+	// 	return
+	// }
 	c.String(http.StatusOK, "Face Registered!")
 }
